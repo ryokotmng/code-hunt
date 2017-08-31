7bd1ce23c18daf0e461ca59b807a0538f1f03551
ryokotmng <ryokotmng@gmail.com>
2017-08-31 16:20:27 +0900

    landing page works

--- server/express-server.js
+++ server/express-server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use('/', express.static('/public'));
+app.use('/', express.static('./public'));
 
 var server = app.listen(3000, function(){
   console.log('Server is running...');
