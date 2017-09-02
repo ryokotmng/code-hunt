aa5eaf66be552b113ab2e9719ac36a4c06e7053a
ryokotmng <ryokotmng@gmail.com>
2017-09-02 16:52:36 +0900

    post popup page added

--- src/react-components/Navbar/LoginPopup.js
+++ src/react-components/Navbar/LoginPopup.js
@@ -4,7 +4,7 @@ import Popup from './Popup';
 class LoginPopup extends React.Component {
   render() {
     return (
-      <Popup {...this.props}>
+      <Popup {...this.props} style="login-popup">
         <img src="https://fontmeme.com/permalink/170902/8a48eabdae4397362dc37cf8e0951858.png"/>
         <h1>Login to Join The Community</h1>
         <p>CodeHunt is a Community to share and geek out about the latest code, podcast and news. Join us :)</p>
